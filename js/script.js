b2f24f524ebd04b1c8d2e9abe8b4143b3964fa51
tinytrack <janem1211@yandex.ru>
2019-03-13 23:04:29 +0300

    Начала разработку корзины

--- js/script.js
+++ js/script.js
@@ -8,28 +8,30 @@ ready(function () {
         mainNav.classList.toggle('main-nav--open');
     });
 
-    let filterBtn = document.querySelector('#filters-trigger');
-    let filtersWrapper = document.querySelector('#filters');
-    filterBtn.addEventListener('click', function () {
-        filtersWrapper.classList.toggle('filters--open');
-    });
+    let pageHref = document.location.href;
 
-    const cardTemplate = document.querySelector('#card__item');
-    const newCardFragment = document.createDocumentFragment();
-    for (i = 0; i < 10; i++) {
-        const newCard = cardTemplate.content.cloneNode(true);
-        newCard.querySelector('.card__inner').href = `index.html#${books[i].uri}`;
-        newCard.querySelector('.card__img').src = `img/books/${books[i].uri}.jpg`;
-        newCard.querySelector('.card__img').alt = `${books[i].name}`;
-        newCard.querySelector('.card__title').textContent = `${books[i].name}`;
-        newCard.querySelector('.card__price').textContent = `${books[i].price} ₽`;
-        newCardFragment.appendChild(newCard);
-    }
+    if (pageHref.includes('index')) {
+        let filterBtn = document.querySelector('#filters-trigger');
+        let filtersWrapper = document.querySelector('#filters');
+        filterBtn.addEventListener('click', function () {
+            filtersWrapper.classList.toggle('filters--open');
+        });
 
-    document.querySelector('.catalog__books-list').appendChild(newCardFragment);
+        const cardTemplate = document.querySelector('#card__item');
+        const newCardFragment = document.createDocumentFragment();
+        for (i = 0; i < 10; i++) {
+            const newCard = cardTemplate.content.cloneNode(true);
+            newCard.querySelector('.card__inner').href = `index.html#${books[i].uri}`;
+            newCard.querySelector('.card__img').src = `img/books/${books[i].uri}.jpg`;
+            newCard.querySelector('.card__img').alt = `${books[i].name}`;
+            newCard.querySelector('.card__title').textContent = `${books[i].name}`;
+            newCard.querySelector('.card__price').textContent = `${books[i].price} ₽`;
+            newCardFragment.appendChild(newCard);
+        }
 
+        document.querySelector('.catalog__books-list').appendChild(newCardFragment);
 
-    initPopup = (function () {
+        initPopup = (function () {
         let isPopupShown = false;
         let htmlTag = document.querySelector('.page');
         let popupContainer = document.querySelector('#modal-book-view');
@@ -114,113 +116,115 @@ ready(function () {
 
             document.querySelector('.modal__content').appendChild(newPopupFragment);
         }
-    }());
+        }());
 
+        initFilter = (function () {
+            let searchBtn = document.querySelector('#books-show-btn');
+            let formFilters = document.querySelector('#filters-form');
 
-    initFilter = (function () {
-        let searchBtn = document.querySelector('#books-show-btn');
-        let formFilters = document.querySelector('#filters-form');
+            function handle(event) {
+                let searchResult = [];
+                let inputNameVal = document.querySelector('[name=book-name]').value;
+                let inputAuthorVal = document.querySelector('[name=author]').value;
+                let inputPublishVal = document.querySelector('[name=publisher]').value;
+                let selectYearFromVal = document.querySelector('[name=year-from]').getElementsByTagName('option')[0].value;
+                let selectYearToVal = document.querySelector('[name=year-to]').getElementsByTagName('option')[0].value;
+                let selectCoverVal = document.querySelector('[name=binding]').getElementsByTagName('option')[0].value;
+                let selectLangVal = document.querySelector('[name=language]').getElementsByTagName('option')[0].value;
+                let inputPriceFromVal = document.querySelector('#price-from').value;
+                let inputPriceToVal = document.querySelector('#price-to').value;
 
-        function handle(event) {
-            let searchResult = [];
-            let inputNameVal = document.querySelector('[name=book-name]').value;
-            let inputAuthorVal = document.querySelector('[name=author]').value;
-            let inputPublishVal = document.querySelector('[name=publisher]').value;
-            let selectYearFromVal = document.querySelector('[name=year-from]').getElementsByTagName('option')[0].value;
-            let selectYearToVal = document.querySelector('[name=year-to]').getElementsByTagName('option')[0].value;
-            let selectCoverVal = document.querySelector('[name=binding]').getElementsByTagName('option')[0].value;
-            let selectLangVal = document.querySelector('[name=language]').getElementsByTagName('option')[0].value;
-            let inputPriceFromVal = document.querySelector('#price-from').value;
-            let inputPriceToVal = document.querySelector('#price-to').value;
-
-            if (event.target.dataset.bind === 'filter-element') {
-                books.forEach(function (book) {
-                    //проверка на имя
-                    if (inputNameVal !== "") {
-                        let re = new RegExp(inputNameVal, 'gi');
-                        if (re.test(book.name) == false) {
-                            return;
-                        };
-                    }
-                    //проверка на автора
-                    if (inputAuthorVal !== "") {
-                        let re = new RegExp(inputAuthorVal, 'gi');
-                        if (re.test(book.author) == false) {
-                            return;
-                        };
-                    }
-                    //проверка на издателя
-                    if (inputPublishVal !== "") {
-                        let re = new RegExp(inputPublishVal, 'gi');
-                        if (re.test(book.publishing) == false) {
-                            return;
-                        };
-                    }
-                    //проверка на дату публикации
-                    if (((book.year >= selectYearFromVal) && (book.year <= selectYearToVal)) === false) {
-                        return;
-                    }
-                    //проверка на обложку
-                    if (selectCoverVal !== book.cover) {
-                        return;
-                    }
-                    //проверка на язык
-                    if (selectLangVal !== book.lang) {
-                        return;
-                    }
-                    //проверка по цене
-                    if ((inputPriceFromVal !== "") && (inputPriceToVal !== "")) {
-                        if (((book.price >= inputPriceFromVal) && (book.price <= inputPriceToVal)) === false) {
+                if (event.target.dataset.bind === 'filter-element') {
+                    books.forEach(function (book) {
+                        //проверка на имя
+                        if (inputNameVal !== "") {
+                            let re = new RegExp(inputNameVal, 'gi');
+                            if (re.test(book.name) == false) {
+                                return;
+                            };
+                        }
+                        //проверка на автора
+                        if (inputAuthorVal !== "") {
+                            let re = new RegExp(inputAuthorVal, 'gi');
+                            if (re.test(book.author) == false) {
+                                return;
+                            };
+                        }
+                        //проверка на издателя
+                        if (inputPublishVal !== "") {
+                            let re = new RegExp(inputPublishVal, 'gi');
+                            if (re.test(book.publishing) == false) {
+                                return;
+                            };
+                        }
+                        //проверка на дату публикации
+                        if (((book.year >= selectYearFromVal) && (book.year <= selectYearToVal)) === false) {
                             return;
                         }
+                        //проверка на обложку
+                        if (selectCoverVal !== book.cover) {
+                            return;
+                        }
+                        //проверка на язык
+                        if (selectLangVal !== book.lang) {
+                            return;
+                        }
+                        //проверка по цене
+                        if ((inputPriceFromVal !== "") && (inputPriceToVal !== "")) {
+                            if (((book.price >= inputPriceFromVal) && (book.price <= inputPriceToVal)) === false) {
+                                return;
+                            }
+                        }
+                        searchResult.push(book);
+                    });
+
+                    document.querySelector('#books-num').textContent = searchResult.length;
+                    if (searchResult.length !== 0) {
+                        document.querySelector('#books-show-btn').disabled = false;
+                    } else {
+                        document.querySelector('#books-show-btn').disabled = true;
                     }
-                    searchResult.push(book);
-                });
-
-                document.querySelector('#books-num').textContent = searchResult.length;
-                if (searchResult.length !== 0) {
-                    document.querySelector('#books-show-btn').disabled = false;
-                } else {
-                    document.querySelector('#books-show-btn').disabled = true;
                 }
+                submitBtn(searchResult);
             }
-            submitBtn(searchResult);
-        }
 
-        function submitBtn(filteredArray) {
-            searchBtn.addEventListener('click', function (e) {
-                e.preventDefault();
-                renderCards(filteredArray)
-            });
-        }
+            function submitBtn(filteredArray) {
+                searchBtn.addEventListener('click', function (e) {
+                    e.preventDefault();
+                    renderCards(filteredArray)
+                });
+            }
 
-        formFilters.addEventListener('change', handle);
-        formFilters.addEventListener('input', handle);
-    }());
+            formFilters.addEventListener('change', handle);
+            formFilters.addEventListener('input', handle);
+        }());
 
-    function renderCards(arr) {
-        let myNode = document.querySelector(".catalog__books-list");
-        myNode.innerHTML = '';
-        for (i = 0; i < arr.length; i++) {
-            const newCard = cardTemplate.content.cloneNode(true);
-            newCard.querySelector('.card__inner').href = `index.html#${arr[i].uri}`;
-            newCard.querySelector('.card__img').src = `img/books/${arr[i].uri}.jpg`;
-            newCard.querySelector('.card__img').alt = `${arr[i].name}`;
-            newCard.querySelector('.card__title').textContent = `${arr[i].name}`;
-            newCard.querySelector('.card__price').textContent = `${arr[i].price} ₽`;
-            newCardFragment.appendChild(newCard);
-        }
+        function renderCards(arr) {
+            let myNode = document.querySelector(".catalog__books-list");
+            myNode.innerHTML = '';
+            for (i = 0; i < arr.length; i++) {
+                const newCard = cardTemplate.content.cloneNode(true);
+                newCard.querySelector('.card__inner').href = `index.html#${arr[i].uri}`;
+                newCard.querySelector('.card__img').src = `img/books/${arr[i].uri}.jpg`;
+                newCard.querySelector('.card__img').alt = `${arr[i].name}`;
+                newCard.querySelector('.card__title').textContent = `${arr[i].name}`;
+                newCard.querySelector('.card__price').textContent = `${arr[i].price} ₽`;
+                newCardFragment.appendChild(newCard);
+            }
 
-        document.querySelector('.catalog__books-list').appendChild(newCardFragment);
-    };
-
+            document.querySelector('.catalog__books-list').appendChild(newCardFragment);
+        };
+    }
+    
     initHeaderFilter = (function () {
         headerFilterItems = document.querySelectorAll('.tabs__item');
         headerFilterItems.forEach(function (item) {
             item.addEventListener('click', function (event) {
                 event.preventDefault();
                 activeElem = document.querySelector('.tabs__item--active');
-                activeElem.classList.remove('tabs__item--active')
+                if (activeElem !== null) {
+                    activeElem.classList.remove('tabs__item--active')
+                }
                 document.querySelector('#filters-form').reset();
                 let headerFilterArr = [];
                 let target = event.target;
@@ -235,8 +239,6 @@ ready(function () {
             });
         });
     }());
-
-    ///
 
     initCartArr = (function () {
         let catalogBuyBtns = document.querySelectorAll('.card__buy');
@@ -286,6 +288,11 @@ ready(function () {
         }
         
     }());
+
+    console.log(cartArrStorage)
+
+
+
 
     // ВНИМАНИЕ!
     // Нижеследующий код (кастомный селект и выбор диапазона цены) работает
